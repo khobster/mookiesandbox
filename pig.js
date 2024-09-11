fd02aa7d1cd3e3c4183a72a31a91e8476699305b
khobster <152924497+khobster@users.noreply.github.com>
2024-09-10 22:57:59 -0400

    Update pig.js

--- pig.js
+++ pig.js
@@ -91,7 +91,7 @@ function displayPlayerFromDecade(decade) {
         else if (playerYear >= 60 && playerYear <= 69) playerDecade = '1960s';
         else if (playerYear >= 70 && playerYear <= 79) playerDecade = '1970s';
         else if (playerYear >= 80 && playerYear <= 89) playerDecade = '1980s';
-        else if (playerYear >= 90 and playerYear <= 99) playerDecade = '1990s';
+        else if (playerYear >= 90 && playerYear <= 99) playerDecade = '1990s';
         else if (playerYear >= 0 && playerYear <= 9) playerDecade = '2000s';
         else if (playerYear >= 10 && playerYear <= 19) playerDecade = '2010s';
         else if (playerYear >= 20 && playerYear <= 29) playerDecade = '2020s';
